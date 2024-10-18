1f3f816bb1df5fcc6c6a84bfb9d95bc3b94d6c5f
Sharad Mathukiya <saradmathukiya9@gmail.com>
2024-10-18 10:07:16 +0530

    constant changes

--- frontend/src/utils/constant.js
+++ frontend/src/utils/constant.js
@@ -1,4 +1,4 @@
-const BASE_URL = process.env.REACT_APP_BASE_URL;
+const BASE_URL = import.meta.env.VITE_BASE_URL;
 
 export const USER_API_END_POINT = BASE_URL + "/api/v1/user";
 export const JOB_API_END_POINT = BASE_URL + "/api/v1/job";
