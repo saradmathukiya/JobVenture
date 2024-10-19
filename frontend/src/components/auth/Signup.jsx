3eb1ee3d194cf6b66c4f7d4f84e81f0c8b0964ef
Sharad Mathukiya <saradmathukiya9@gmail.com>
2024-10-19 13:29:58 +0530

    backend api changes

--- frontend/src/components/auth/Signup.jsx
+++ frontend/src/components/auth/Signup.jsx
@@ -47,8 +47,7 @@ const Signup = () => {
         try {
             dispatch(setLoading(true));
             const res = await axios.post(`${USER_API_END_POINT}/register`, formData, {
-                headers: { 'Content-Type': "multipart/form-data" },
-                withCredentials: true,
+                headers: { 'Content-Type': "multipart/form-data" }
             });
             if (res.data.success) {
                 navigate("/login");
--- frontend/src/utils/constant.js
+++ frontend/src/utils/constant.js
@@ -1,6 +1,8 @@
-const isDevelopment = import.meta.env.DEV;
+// const isDevelopment = import.meta.env.DEV;
 
-const BASE_URL = isDevelopment ? "/api" : import.meta.env.VITE_BASE_URL;
+// const BASE_URL = isDevelopment ? "/api" : import.meta.env.VITE_BASE_URL;
+
+const BASE_URL = import.meta.env.VITE_BASE_URL;
 
 export const USER_API_END_POINT = `${BASE_URL}/v1/user`;
 export const JOB_API_END_POINT = `${BASE_URL}/v1/job`;
